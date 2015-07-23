aa9902182b713f55f878dd575e7299fe9523c847
Nicholas Smith <npsmith1990@gmail.com>
2015-07-22 23:22:29 -0500

    Automatically remove asterisks

--- gravity_forms_placeholders.js
+++ gravity_forms_placeholders.js
@@ -2,18 +2,21 @@
 
 // Form Elements
 $('form[id^=gform]').each(function(id,el) {
-$(el).find('.gfield_label').each(function(lbl_id,lbl_el) {
-  var text;
-  text = $(lbl_el).text();
-  $input = $(lbl_el).next().find('>input');
-  $textarea = $(lbl_el).next().find('textarea');
-  if ($input.length <= 0) {
-    $input = $(lbl_el).prev();
-  }
-    $input.attr('placeholder',text);
-    $textarea.attr('placeholder',text);
-    $(lbl_el).addClass('hidden');
-});
+  $(el).find('.gfield_label').each(function(lbl_id,lbl_el) {
+    var text;
+    text = $(lbl_el).text();
+    if (text.contains('*')) {// Get rid of *'s for required fields
+      text = text.substr(0,text.length-1);
+    }
+    $input = $(lbl_el).next().find('>input');
+    $textarea = $(lbl_el).next().find('textarea');
+    if ($input.length <= 0) {
+      $input = $(lbl_el).prev();
+    }
+      $input.attr('placeholder',text);
+      $textarea.attr('placeholder',text);
+      $(lbl_el).addClass('hidden');
+  });
 });
 // Address Container
 $('.ginput_container > span').each(function(id,el){
